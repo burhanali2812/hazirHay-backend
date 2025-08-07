e59f8bb095803f294ae829e35aad6f22a4b7cb34
burhanali2812 <syedburhanali830@gmail.com>
2025-08-08 03:08:06 +0500

    hazir hay backend log

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("HazirHay Backend is Live!");
+});
+
 
 const connectDB = async () => {
   try {
