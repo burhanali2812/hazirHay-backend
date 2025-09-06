d0c758f0fb90b45871586581616638bd36a4728e
burhanali2812 <syedburhanali830@gmail.com>
2025-09-07 03:42:43 +0500

    cart data get route

--- routes/CartRoutes.js
+++ routes/CartRoutes.js
@@ -31,5 +31,33 @@ router.post("/saveCartData", authMiddleWare, async (req, res) => {
   }
 });
 
+router.get("/getCartData", authMiddleWare, async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const cartItems = await Cart.find({ userId }); 
+
+    if (!cartItems || cartItems.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Your cart is empty!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Cart data found",
+      data: cartItems,
+    });
+  } catch (error) {
+    console.error("Error fetching cart:", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server error while fetching cart",
+    });
+  }
+});
+
+
 
 module.exports = router;
