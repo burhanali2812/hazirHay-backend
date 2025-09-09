9ede1d5e2e0f309145b22c38e8f81d2a9ae07303
burhanali2812 <syedburhanali830@gmail.com>
2025-09-10 00:24:31 +0500

    order details routes

--- routes/RequestRoutes.js
+++ routes/RequestRoutes.js
@@ -5,59 +5,22 @@ const express = require("express");
 const router = express.Router();
 
 
-router.post("/sendRequestData", authMiddleWare, async (req, res) => {
+router.post("/sendBulkRequests", authMiddleWare, async (req, res) => {
   try {
-    const {category,subCategory, location ,userId} = req.body;
+    const {requests} = req.body;
 
+    const saved =[];
 
-    if (!userId || !category || !subCategory || !location || !location.coordinates) {
-      return res.status(400).json({
-        success: false,
-        message: "Missing required fields (userId, category, subCategory, location)",
-      });
+    for(const reqData of requests){
+      const newRequests = new Requests(reqData);
+       await newRequests.save();
+       saved.push(newRequests);
     }
-
-    const liveProviders = await ShopDetails.find({ isLive: true });
-    if (!liveProviders.length) {
-      return res.status(404).json({
-        success: false,
-        message: "No online providers found",
-      });
-    }
-
-    const categoryProvider = liveProviders.filter(
-      (provider) =>
-        Array.isArray(provider.servicesOffered) &&
-        provider.servicesOffered.some(
-          (service) => service.category === category
-        )
-    );
-
-    if (!categoryProvider.length) {
-      return res.status(404).json({
-        success: false,
-        message: `No providers found for category: ${category}`,
-      });
-    }
-    await Promise.all(
-      categoryProvider.map((provider) =>
-        new Requests({
-          shopId: provider._id,
-          userId,
-          shopOwnerId : provider.owner,
-          location: {
-            coordinates: location.coordinates,
-            area: location.area,
-          },
-          category,
-          subCategory,
-        }).save()
-      )
-    );
 
     res.status(200).json({
       success: true,
-      message: "Request sent to matching providers.",
+      message: "Request sent toproviders.",
+      requests : saved
     });
   } catch (error) {
     console.error("Error finding providers:", error);
