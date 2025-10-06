0ac3a4560ef0db81e68a21c8171ddc3d874aaa44
burhanali2812 <syedburhanali830@gmail.com>
2025-10-06 15:48:04 +0500

    dashboard

--- routes/RequestRoutes.js
+++ routes/RequestRoutes.js
@@ -184,6 +184,30 @@ router.delete("/deleteRequests", async(req,res)=>{
     res.json({message : "internal server error"})
   }
 })
+router.get("/getshopRequest/:id", authMiddleWare, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const requests = await Requests.find({ shopOwnerId: id })
+      .populate("userId", "name phone email profilePicture")
+      .sort({ createdAt: -1 })
+      .lean();
+    if (!requests || requests.length === 0) {
+      return res
+        .status(404)
+        .json({
+          success: false,
+          message: "No requests found for this shop owner",
+        });
+    }
+
+    res.status(200).json({ success: true, data: requests });
+  } catch (error) {
+    console.error("Error fetching Shopkeeper requests:", error);
+    console.error("Error fetching Shopkeeper requests:", error.stack);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
 
 
 module.exports = router;
