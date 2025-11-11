ad6da41386d487bbd76bcb417714f07e57e1f915
burhanali2812 <syedburhanali830@gmail.com>
2025-11-11 14:40:59 +0500

    set backend

--- models/ShopKeeper.js
+++ models/ShopKeeper.js
@@ -8,24 +8,12 @@ const shopKeeperSchema = new mongoose.Schema({
   phone: { type: String, required: true },
   cnic: { type: String, required: true },
   address: { type: String, required: true },
-   profilePicture:{type:String, required : true},
-   verificationDocument:{type:String, required : true},
+   profilePicture:{type:String},
   isVerified: { type: Boolean, default : false },
-  isBusy: { type: Boolean, default : false },
   isShop: { type: Boolean, default : false },
   isLive: { type: Boolean, default : false },
-  location: {
-    coordinates: {
-      type: [Number],
-    },
-    area: {
-      type: String,
-    },
-  },
   createdAt: { type: Date, default: Date.now },
   activityCount: { type: Number, default: 0 },
-
-  socketId: { type: String, default: null },
 
 });
 
