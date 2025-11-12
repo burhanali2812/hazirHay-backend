8087476d7012755473d4ca329d8433882a42f61a
burhanali2812 <syedburhanali830@gmail.com>
2025-11-12 14:31:32 +0500

    set model issue

--- routes/TransactionRoutes.js
+++ routes/TransactionRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authMiddleWare = require("../authMiddleWare");
 
-const Transaction = require("../models/Transaction");
+const Transaction = require("../models/Transactions");
 
 router.post("/createTransaction", authMiddleWare, async (req, res) => {
   try {
